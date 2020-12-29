ac32b795e72568a0d8ec6fd7ecdc306123df6f97
dung1764 <dung1764@hotmail.com>
2020-12-29 21:48:38 +0800

    add animation of the player

--- main.ts
+++ main.ts
@@ -3,25 +3,331 @@ forever(() => {
     music.playMelody("E F B A E A D - ", 120)
 })
 
+//controller
+ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    hero,
+    [img`
+        . . . . . . f f f f . . . . . .
+        . . . . f f e e e e f f . . . .
+        . . . f e e e f f e e e f . . .
+        . . f f f f f 2 2 f f f f f . .
+        . . f f e 2 e 2 2 e 2 e f f . .
+        . . f e 2 f 2 f f 2 f 2 e f . .
+        . . f f f 2 2 e e 2 2 f f f . .
+        . f f e f 2 f e e f 2 f e f f .
+        . f e e f f e e e e f e e e f .
+        . . f e e e e e e e e e e f . .
+        . . . f e e e e e e e e f . . .
+        . . e 4 f f f f f f f f 4 e . .
+        . . 4 d f 2 2 2 2 2 2 f d 4 . .
+        . . 4 4 f 4 4 4 4 4 4 f 4 4 . .
+        . . . . . f f f f f f . . . . .
+        . . . . . f f . . f f . . . . .
+    `,img`
+        . . . . . . . . . . . . . . . .
+        . . . . . . f f f f . . . . . .
+        . . . . f f e e e e f f . . . .
+        . . . f e e e f f e e e f . . .
+        . . . f f f f 2 2 f f f f . . .
+        . . f f e 2 e 2 2 e 2 e f f . .
+        . . f e 2 f 2 f f f 2 f e f . .
+        . . f f f 2 f e e 2 2 f f f . .
+        . . f e 2 f f e e 2 f e e f . .
+        . f f e f f e e e f e e e f f .
+        . f f e e e e e e e e e e f f .
+        . . . f e e e e e e e e f . . .
+        . . . e f f f f f f f f 4 e . .
+        . . . 4 f 2 2 2 2 2 e d d 4 . .
+        . . . e f f f f f f e e 4 . . .
+        . . . . f f f . . . . . . . . .
+    `,img`
+            . . . . . . f f f f . . . . . .
+            . . . . f f e e e e f f . . . .
+            . . . f e e e f f e e e f . . .
+            . . f f f f f 2 2 f f f f f . .
+            . . f f e 2 e 2 2 e 2 e f f . .
+            . . f e 2 f 2 f f 2 f 2 e f . .
+            . . f f f 2 2 e e 2 2 f f f . .
+            . f f e f 2 f e e f 2 f e f f .
+            . f e e f f e e e e f e e e f .
+            . . f e e e e e e e e e e f . .
+            . . . f e e e e e e e e f . . .
+            . . e 4 f f f f f f f f 4 e . .
+            . . 4 d f 2 2 2 2 2 2 f d 4 . .
+            . . 4 4 f 4 4 4 4 4 4 f 4 4 . .
+            . . . . . f f f f f f . . . . .
+            . . . . . f f . . f f . . . . .
+        `,img`
+            . . . . . . . . . . . . . . . .
+            . . . . . . f f f f . . . . . .
+            . . . . f f e e e e f f . . . .
+            . . . f e e e f f e e e f . . .
+            . . . f f f f 2 2 f f f f . . .
+            . . f f e 2 e 2 2 e 2 e f f . .
+            . . f e f 2 f f f 2 f 2 e f . .
+            . . f f f 2 2 e e f 2 f f f . .
+            . . f e e f 2 e e f f 2 e f . .
+            . f f e e e f e e e f f e f f .
+            . f f e e e e e e e e e e f f .
+            . . . f e e e e e e e e f . . .
+            . . e 4 f f f f f f f f e . . .
+            . . 4 d d e 2 2 2 2 2 f 4 . . .
+            . . . 4 e e f f f f f f e . . .
+            . . . . . . . . . f f f . . . .
+        `],
+    200,
+    true
+    )
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    hero,
+    [img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . f f e 2 f f f f f f 2 e f f . 
+        . f f f f f e e e e f f f f f . 
+        . . f e f b f 4 4 f b f e f . . 
+        . . f e 4 1 f d d f 1 4 e f . . 
+        . . . f e 4 d d d d 4 e f e . . 
+        . . f e f 2 2 2 2 e d d 4 e . . 
+        . . e 4 f 2 2 2 2 e d d e . . . 
+        . . . . f 4 4 5 5 f e e . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f . . . . . . . . . 
+        `,img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f e e 2 2 2 2 2 2 e f f . . 
+        . f f e 2 f f f f f f 2 e f f . 
+        . f f f f f e e e e f f f f f . 
+        . . f e f b f 4 4 f b f e f . . 
+        . . f e 4 1 f d d f 1 4 e f . . 
+        . . e f e 4 d d d d 4 e f . . . 
+        . . e 4 d d e 2 2 2 2 f e f . . 
+        . . . e d d e 2 2 2 2 f 4 e . . 
+        . . . . e e f 5 5 4 4 f . . . . 
+        . . . . . f f f f f f f . . . . 
+        . . . . . . . . . f f f . . . . 
+        `],
+    200,
+    true
+    )
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    hero,
+    [img`
+        . . . . f f f f f f . . . . . .
+        . . . f 2 f e e e e f f . . . .
+        . . f 2 2 2 f e e e e f f . . .
+        . . f e e e e f f e e e f . . .
+        . f e 2 2 2 2 e e f f f f . . .
+        . f 2 e f f f f 2 2 2 e f . . .
+        . f f f e e e f f f f f f f . .
+        . f e e 4 4 f b e 4 4 e f f . .
+        . . f e d d f 1 4 d 4 e e f . .
+        . . . f d d d d 4 e e e f . . .
+        . . . f e 4 4 4 e e f f . . . .
+        . . . f 2 2 2 e d d 4 . . . . .
+        . . . f 2 2 2 e d d e . . . . .
+        . . . f 5 5 4 f e e f . . . . .
+        . . . . f f f f f f . . . . . .
+        . . . . . . f f f . . . . . . .
+    `,img`
+        . . . . . . . . . . . . . . . .
+        . . . . f f f f f f . . . . . .
+        . . . f 2 f e e e e f f . . . .
+        . . f 2 2 2 f e e e e f f . . .
+        . . f e e e e f f e e e f . . .
+        . f e 2 2 2 2 e e f f f f . . .
+        . f 2 e f f f f 2 2 2 e f . . .
+        . f f f e e e f f f f f f f . .
+        . f e e 4 4 f b e 4 4 e f f . .
+        . . f e d d f 1 4 d 4 e e f . .
+        . . . f d d d e e e e e f . . .
+        . . . f e 4 e d d 4 f . . . . .
+        . . . f 2 2 e d d e f . . . . .
+        . . f f 5 5 f e e f f f . . . .
+        . . f f f f f f f f f f . . . .
+        . . . f f f . . . f f . . . . .
+    `,img`
+        . . . . f f f f f f . . . . . .
+        . . . f 2 f e e e e f f . . . .
+        . . f 2 2 2 f e e e e f f . . .
+        . . f e e e e f f e e e f . . .
+        . f e 2 2 2 2 e e f f f f . . .
+        . f 2 e f f f f 2 2 2 e f . . .
+        . f f f e e e f f f f f f f . .
+        . f e e 4 4 f b e 4 4 e f f . .
+        . . f e d d f 1 4 d 4 e e f . .
+        . . . f d d d d 4 e e e f . . .
+        . . . f e 4 4 4 e e f f . . . .
+        . . . f 2 2 2 e d d 4 . . . . .
+        . . . f 2 2 2 e d d e . . . . .
+        . . . f 5 5 4 f e e f . . . . .
+        . . . . f f f f f f . . . . . .
+        . . . . . . f f f . . . . . . .
+    `,img`
+        . . . . . . . . . . . . . . . .
+        . . . . f f f f f f . . . . . .
+        . . . f 2 f e e e e f f . . . .
+        . . f 2 2 2 f e e e e f f . . .
+        . . f e e e e f f e e e f . . .
+        . f e 2 2 2 2 e e f f f f . . .
+        . f 2 e f f f f 2 2 2 e f . . .
+        . f f f e e e f f f f f f f . .
+        . f e e 4 4 f b e 4 4 e f f . .
+        . . f e d d f 1 4 d 4 e e f . .
+        . . . f d d d d 4 e e e f . . .
+        . . . f e 4 4 4 e d d 4 . . . .
+        . . . f 2 2 2 2 e d d e . . . .
+        . . f f 5 5 4 4 f e e f . . . .
+        . . f f f f f f f f f f . . . .
+        . . . f f f . . . f f . . . . .
+    `],
+    200,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    hero,
+    [img`
+        . . . . . . f f f f f f . . . .
+        . . . . f f e e e e f 2 f . . .
+        . . . f f e e e e f 2 2 2 f . .
+        . . . f e e e f f e e e e f . .
+        . . . f f f f e e 2 2 2 2 e f .
+        . . . f e 2 2 2 f f f f e 2 f .
+        . . f f f f f f f e e e f f f .
+        . . f f e 4 4 e b f 4 4 e e f .
+        . . f e e 4 d 4 1 f d d e f . .
+        . . . f e e e 4 d d d d f . . .
+        . . . . f f e e 4 4 4 e f . . .
+        . . . . . 4 d d e 2 2 2 f . . .
+        . . . . . e d d e 2 2 2 f . . .
+        . . . . . f e e f 4 5 5 f . . .
+        . . . . . . f f f f f f . . . .
+        . . . . . . . f f f . . . . . .
+    `,img`
+        . . . . . . . . . . . . . . . .
+        . . . . . . f f f f f f . . . .
+        . . . . f f e e e e f 2 f . . .
+        . . . f f e e e e f 2 2 2 f . .
+        . . . f e e e f f e e e e f . .
+        . . . f f f f e e 2 2 2 2 e f .
+        . . . f e 2 2 2 f f f f e 2 f .
+        . . f f f f f f f e e e f f f .
+        . . f f e 4 4 e b f 4 4 e e f .
+        . . f e e 4 d 4 1 f d d e f . .
+        . . . f e e e 4 d d d d f . . .
+        . . . . 4 d d e 4 4 4 e f . . .
+        . . . . e d d e 2 2 2 2 f . . .
+        . . . . f e e f 4 4 5 5 f f . .
+        . . . . f f f f f f f f f f . .
+        . . . . . f f . . . f f f . . .
+    `,img`
+        . . . . . . f f f f f f . . . .
+        . . . . f f e e e e f 2 f . . .
+        . . . f f e e e e f 2 2 2 f . .
+        . . . f e e e f f e e e e f . .
+        . . . f f f f e e 2 2 2 2 e f .
+        . . . f e 2 2 2 f f f f e 2 f .
+        . . f f f f f f f e e e f f f .
+        . . f f e 4 4 e b f 4 4 e e f .
+        . . f e e 4 d 4 1 f d d e f . .
+        . . . f e e e 4 d d d d f . . .
+        . . . . f f e e 4 4 4 e f . . .
+        . . . . . 4 d d e 2 2 2 f . . .
+        . . . . . e d d e 2 2 2 f . . .
+        . . . . . f e e f 4 5 5 f . . .
+        . . . . . . f f f f f f . . . .
+        . . . . . . . f f f . . . . . .
+    `,img`
+        . . . . . . . . . . . . . . . .
+        . . . . . . f f f f f f . . . .
+        . . . . f f e e e e f 2 f . . .
+        . . . f f e e e e f 2 2 2 f . .
+        . . . f e e e f f e e e e f . .
+        . . . f f f f e e 2 2 2 2 e f .
+        . . . f e 2 2 2 f f f f e 2 f .
+        . . f f f f f f f e e e f f f .
+        . . f f e 4 4 e b f 4 4 e e f .
+        . . f e e 4 d 4 1 f d d e f . .
+        . . . f e e e e e d d d f . . .
+        . . . . . f 4 d d e 4 e f . . .
+        . . . . . f e d d e 2 2 f . . .
+        . . . . f f f e e f 5 5 f f . .
+        . . . . f f f f f f f f f f . .
+        . . . . . f f . . . f f f . . .
+    `],
+    200,
+    true
+    )
+})
+
 let hero = sprites.create(img`
-    . . . . . f f f f . . . . .
-    . . . f f 5 5 5 5 f f . . .
-    . . f 5 5 5 5 5 5 5 5 f . .
-    . f 5 5 5 5 5 5 5 5 5 5 f .
-    . f 5 5 5 d b b d 5 5 5 f .
-    f 5 5 5 b 4 4 4 4 b 5 5 5 f
-    f 5 5 c c 4 4 4 4 c c 5 5 f
-    f b b f b f 4 4 f b f b b f
-    f b b 4 1 f d d f 1 4 b b f
-    . f b f d d d d d d f b f .
-    . f e f e 4 4 4 4 e f e f .
-    . e 4 f 6 9 9 9 9 6 f 4 e .
-    . 4 d c 9 9 9 9 9 9 c d 4 .
-    . 4 f b 3 b 3 b 3 b b f 4 .
-    . . f f 3 b 3 b 3 3 f f . .
-    . . . . f f b b f f . . . .
+    . . . . . . f f f f . . . . . .
+    . . . . f f f 2 2 f f f . . . .
+    . . . f f f 2 2 2 2 f f f . . .
+    . . f f f e e e e e e f f f . .
+    . . f f e 2 2 2 2 2 2 e e f . .
+    . . f e 2 f f f f f f 2 e f . .
+    . . f f f f e e e e f f f f . .
+    . f f e f b f 4 4 f b f e f f .
+    . f e e 4 1 f d d f 1 4 e e f .
+    . . f e e d d d d d d e e f . .
+    . . . f e e 4 4 4 4 e e f . . .
+    . . e 4 f 2 2 2 2 2 2 f 4 e . .
+    . . 4 d f 2 2 2 2 2 2 f d 4 . .
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . .
+    . . . . . f f f f f f . . . . .
+    . . . . . f f . . f f . . . . .
 `, SpriteKind.Player)
-controller.moveSprite(hero, 100, 100)
+controller.moveSprite(hero, 50, 50)
 let currentLevel = 0
 nextLevel()
 
